36e48acf0e4b2582c3b3212f6b1e5c781f35bc97
batch <baaaakkrad@gmail.com>
2019-11-02 23:46:49 +0900

    [fix]Can change 2fragment

--- app/src/main/java/com/batch/avgleclient/collection/CollectionFragment.kt
+++ app/src/main/java/com/batch/avgleclient/collection/CollectionFragment.kt
@@ -1,7 +1,5 @@
 package com.batch.avgleclient.collection
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,11 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.batch.avgleclient.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 class CollectionFragment : Fragment() {
 

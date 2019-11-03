1bfde3b35390294fa1869ef3544a89a54ada0c1b
batch <baaaakkrad@gmail.com>
2019-11-03 19:07:41 +0900

    [bug]Catch same error

--- app/src/main/java/com/batch/avgleclient/category/CategoryViewModel.kt
+++ app/src/main/java/com/batch/avgleclient/category/CategoryViewModel.kt
@@ -5,21 +5,36 @@ import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.batch.avgleclient.model.AvCategory
 import com.batch.avgleclient.repository.AvRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import com.batch.avgleclient.R
 import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class CategoryViewModel(application: Application) : AndroidViewModel(application) {
     var categories = MutableLiveData<List<AvCategory.Response.Category>>()
     private val api = AvRepository(application.applicationContext.getString(R.string.API_AVGLE_URL))
     private val scope = CoroutineScope(Dispatchers.Main)
 
+    init {
+        fetchFromRemote()
+    }
+
     fun fetchFromRemote() {
+//        viewModelScope.launch {
+//            categories.value = api.getAvCategories()
+//        }
         scope.launch {
-            categories.postValue(api.getAvCategories())
+            try {
+                //            categories.postValue(api.getAvCategories())
+                categories.value = api.getAvCategories()
+            } catch (e: Exception) {
+                e.stackTrace
+                Log.d("LOGLOG", e.toString())
+            }
         }
     }
 }

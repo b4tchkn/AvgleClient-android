4387539ac0f462bca1a561be39c88506f1a79757
batch <baaaakkrad@gmail.com>
2019-11-28 22:21:12 +0900

    [fix]Delete no need code

--- app/src/main/java/com/batch/avgleclient/category/CategoryClickListener.kt
+++
@@ -1,8 +0,0 @@
-package com.batch.avgleclient.category
-
-import android.view.View
-import com.batch.avgleclient.model.AvCategory
-
-interface CategoryClickListener {
-    fun onCategoryClicked(category: AvCategory.Response.Category)
-}

472d905d1730de40ef81b31f07157477e15756da
batch <baaaakkrad@gmail.com>
2020-02-02 09:31:47 +0900

    add: create LoginActivity layout

---
+++ app/src/main/java/com/batch/avgleclient/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.batch.avgleclient.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.batch.avgleclient.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}

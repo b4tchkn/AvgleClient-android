09ebe834fbdf74ea3b12b4ce0e3a8a5f72fa4f9e
batch <baaaakkrad@gmail.com>
2020-02-24 02:14:20 +0900

    update: logic and layout in the case of email

--- app/src/main/java/com/batch/avgleclient/login/LoginActivity.kt
+++ app/src/main/java/com/batch/avgleclient/login/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.batch.avgleclient.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.widget.Toast
 import com.batch.avgleclient.R
 import com.google.firebase.auth.FirebaseAuth
@@ -15,7 +17,25 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         auth = FirebaseAuth.getInstance()
-        signin_button.setOnClickListener { signInWithEmail() }
+        signin_button.setOnClickListener {
+            val isEmailEmpty = TextUtils.isEmpty(edit_text_input_email.text)
+            val isPasswordEmpty = TextUtils.isEmpty(edit_text_input_password.text)
+
+            if (!isEmailEmpty && !isPasswordEmpty) {
+                signInWithEmail()
+                text_input_email.error = null
+                text_input_password.error = null
+            } else {
+                when (isEmailEmpty) {
+                    true -> text_input_email.error = getString(R.string.is_empty)
+                    false -> text_input_email.error = null
+                }
+                when (isPasswordEmpty) {
+                    true -> text_input_password.error = getString(R.string.is_empty)
+                    false -> text_input_password.error = null
+                }
+            }
+        }
     }
 
     override fun onStart() {
@@ -24,13 +44,13 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun signInWithEmail() {
-        val email = email_edit_text.text.toString()
-        val password = password_edit_text.text.toString()
+        val email = edit_text_input_email.text.toString()
+        val password = edit_text_input_password.text.toString()
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     val user = auth.currentUser
-                    Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Success!: ${user}", Toast.LENGTH_SHORT).show()
                 } else {
                     Toast.makeText(this, "Failure", Toast.LENGTH_SHORT).show()
                 }

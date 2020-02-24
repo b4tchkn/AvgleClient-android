93e49b4f0d34287933df41510a8aa01015e494ce
batch <baaaakkrad@gmail.com>
2020-02-24 16:52:55 +0900

    update: signin with email and google

--- app/src/main/java/com/batch/avgleclient/login/LoginActivity.kt
+++ app/src/main/java/com/batch/avgleclient/login/LoginActivity.kt
@@ -1,18 +1,27 @@
 package com.batch.avgleclient.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
-import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.batch.avgleclient.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
+import timber.log.Timber
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
     private val viewModel: LoginViewModel by lazy {
         ViewModelProvider(this).get(LoginViewModel::class.java)
     }
@@ -20,16 +29,19 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
         auth = FirebaseAuth.getInstance()
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
         signin_button.setOnClickListener {
             val isEmailEmpty = TextUtils.isEmpty(edit_text_input_email.text)
             val isPasswordEmpty = TextUtils.isEmpty(edit_text_input_password.text)
-
             if (!isEmailEmpty && !isPasswordEmpty) {
-                viewModel.signInWithEmail(this,
-                    auth,
-                    edit_text_input_email.text.toString(),
-                    edit_text_input_password.text.toString())
+                signInWithEmail()
                 text_input_email.error = null
                 text_input_password.error = null
             } else {
@@ -43,10 +55,74 @@ class LoginActivity : AppCompatActivity() {
                 }
             }
         }
+        google_button.setOnClickListener {
+            signInWithGoogle()
+        }
     }
 
-    override fun onStart() {
-        super.onStart()
-        val currentUser = auth.currentUser
+    private fun signInWithEmail() {
+        val email = edit_text_input_email.text.toString()
+        val password = edit_text_input_password.text.toString()
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    Toast.makeText(
+                        applicationContext,
+                        getString(R.string.please_retry),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount?) {
+        val credential = GoogleAuthProvider.getCredential(acct?.idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    Toast.makeText(
+                        applicationContext,
+                        getString(R.string.please_retry),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    private fun signInWithGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account)
+            } catch (e: ApiException) {
+                Timber.tag(TAG + "error").d(e.toString())
+            }
+        }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            Timber.tag(TAG + "name").d(user.displayName)
+            Timber.tag(TAG + "mail").d(user.email)
+            Timber.tag(TAG + "photo").d(user.photoUrl.toString())
+        }
+    }
+
+    companion object {
+        private const val TAG = "AvLogin"
+        private const val RC_SIGN_IN = 123
     }
 }
--- app/src/main/java/com/batch/avgleclient/login/LoginViewModel.kt
+++ app/src/main/java/com/batch/avgleclient/login/LoginViewModel.kt
@@ -2,25 +2,74 @@ package com.batch.avgleclient.login
 
 import android.app.Activity
 import android.app.Application
+import android.content.Intent
 import android.widget.Toast
+import androidx.core.app.ActivityCompat.startActivityForResult
 import androidx.lifecycle.AndroidViewModel
 import com.batch.avgleclient.R
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import timber.log.Timber
+import kotlin.math.sign
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
-    fun signInWithEmail(activity: Activity, auth: FirebaseAuth, email: String, password: String) {
+    fun signInWithEmail(
+        activity: Activity,
+        auth: FirebaseAuth,
+        email: String,
+        password: String): Pair<FirebaseUser?, Boolean> {
+        var user: FirebaseUser? = null
+        var authResult = false
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(activity) { task ->
                 if (task.isSuccessful) {
-                    val user = auth.currentUser
-                    Toast.makeText(getApplication(), user.toString(), Toast.LENGTH_SHORT).show()
+                    user = auth.currentUser
+                    Timber.tag(TAG+"emailSuccess").d(user.toString())
                 } else {
+                    user = null
                     Toast.makeText(
                         getApplication(),
                         getApplication<Application>().getText(R.string.please_retry),
                         Toast.LENGTH_SHORT).show()
                 }
+                authResult = task.isSuccessful
             }
+        Timber.tag(TAG+"email").d(user.toString())
+        return Pair(user, authResult)
+    }
+
+    fun signInWithGoogle(activity: Activity, googleSignInClient: GoogleSignInClient) {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(activity, signInIntent, RC_SIGN_IN, null)
+    }
+
+    fun firebaseAuthWithGoogle(
+        activity: Activity,
+        acct: GoogleSignInAccount?,
+        auth: FirebaseAuth): Pair<FirebaseUser?, Boolean> {
+        var user: FirebaseUser? = null
+        var authResult = false
+        val credential = GoogleAuthProvider.getCredential(acct?.idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(activity) { task ->
+                if (task.isSuccessful) {
+                    user = auth.currentUser
+                    Timber.tag(TAG+"googleSuccess").d(user.toString())
+                } else {
+                    user = null
+                    Timber.tag(TAG+"error").d("失敗")
+                }
+                authResult = task.isSuccessful
+            }
+        Timber.tag(TAG+"google").d(user.toString())
+        return Pair(user, authResult)
+    }
+    companion object {
+        private const val TAG = "AvLogin"
+        private const val RC_SIGN_IN = 123
     }
 }
